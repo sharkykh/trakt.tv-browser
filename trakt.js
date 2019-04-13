b03925a95c773f12371a43aca155ef6fdab26187
sharkykh <sharkykh@gmail.com>
2019-04-13 20:04:26 +0300

    Replace NodeJS's `crypto` with `randombytes`

--- trakt.js
+++ trakt.js
@@ -2,7 +2,7 @@
 
 // requirejs modules
 const ky = require('ky').default;
-const crypto = require('crypto');
+const randomBytes = require('randombytes');
 const methods = require('./methods.json');
 const sanitizer = require('sanitizer').sanitize;
 const pkg = require('./package.json');
@@ -296,7 +296,7 @@ module.exports = class Trakt {
 
     // Get authentication url for browsers
     get_url() {
-        this._authentication.state = crypto.randomBytes(6).toString('hex');
+        this._authentication.state = randomBytes(6).toString('hex');
         // Replace 'api' from the api_url to get the top level trakt domain
         const base_url = this._settings.endpoint.replace(/api\W/, '');
         return `${base_url}/oauth/authorize?response_type=code&client_id=${this._settings.client_id}&redirect_uri=${this._settings.redirect_uri}&state=${this._authentication.state}`;
